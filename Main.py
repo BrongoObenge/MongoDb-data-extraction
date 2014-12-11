11de7277de88f83cc3dbe36d6eb63a3ebc579a8c
Jiar <Jiarshengy@gmail.com>
2014-12-11 14:23:42 +0000

    Finished

--- Main.py
+++ Main.py
@@ -1,3 +1,33 @@
 __author__ = 'j'
-import MongoDbManager
+from manager import MongoDbManager
+import sys
+mongoManager = MongoDbManager.MongoDbManager()
+client = mongoManager.openDb()
+
+coll =  client['Hardware']['hardware-collection']
+cursor =  coll.find()
+
+try:
+    location = sys.argv[1]
+except:
+    location = 'mongodb-dump.php'
+f = open(location,'w')      #Needs to be .php extension for angularJS to interact with
+while cursor.alive:
+   try:
+        f.write(str(cursor.next()) + "\n")
+   except:
+       pass
+
+
+
+
+
+
+
+
+
+
+
+
+
 
--- manager/MongoDbManager.py
+++ manager/MongoDbManager.py
@@ -4,8 +4,10 @@ class MongoDbManager:
     db_url = "mongodb://localhost:27017/"
     db_username = ""
     db_password = ""
+
     def __init__(self):
         pass
+
     def openDb(self):
         client = MongoClient(self.db_url)
         return client
