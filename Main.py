f50a39aa481ef2f29af14a3629bfea8c0c0c8ec7
Jiar <Jiarshengy@gmail.com>
2014-12-15 12:43:08 +0000

    Added an additional filter on ObjectId()

--- Main.py
+++ Main.py
@@ -2,23 +2,31 @@
 __author__ = 'j'
 from manager import MongoDbManager
 import sys
+import re
+
 mongoManager = MongoDbManager.MongoDbManager()
 client = mongoManager.openDb()
 
 coll =  client['Hardware']['hardware-collection']
 cursor =  coll.find()
-
+pattern = r"\"_id\":.ObjectId\(((.)+)\"\)"
 try:
     location = sys.argv[1]
 except:
     location = 'mongodb-dump.php'
 f = open(location,'w')      #Needs to be .php extension for angularJS to interact with
 f.write("[")
+i = 0
 while cursor.alive:
    try:
-       line = str(cursor.next()).replace("\"", "\'")
-       line = line.replace("u\'", "\'")
-       f.write( line + ",\n")
+       line = str(cursor.next()).replace("\'", "\"")
+       line = line.replace("u\"", "\"")
+       try:
+           line =  re.sub(pattern, "" ,line)
+       except:
+           print "no"
+           pass
+       f.write(line + ",\n")
    except:
        pass
 
