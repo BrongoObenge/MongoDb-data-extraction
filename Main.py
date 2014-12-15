e7f9b0255db97838983179614b86f85cdb7ad91f
Jiar <Jiarshengy@gmail.com>
2014-12-15 12:12:23 +0000

    Some small filters for AngularJS support

--- Main.py
+++ Main.py
@@ -1,3 +1,4 @@
+#!/usr/bin/python2.7
 __author__ = 'j'
 from manager import MongoDbManager
 import sys
@@ -12,13 +13,17 @@ try:
 except:
     location = 'mongodb-dump.php'
 f = open(location,'w')      #Needs to be .php extension for angularJS to interact with
+f.write("[")
 while cursor.alive:
    try:
-        f.write(str(cursor.next()) + "\n")
+       line = str(cursor.next()).replace("\"", "\'")
+       line = line.replace("u\'", "\'")
+       f.write( line + ",\n")
    except:
        pass
 
-
+f.write("]")
+client.close()
 
 
 
